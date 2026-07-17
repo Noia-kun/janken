ad54831b0216854c330956dcf97240996cd18e9c
Richwelle Remetio <185665514+Noia-kun@users.noreply.github.com>
2026-07-17 15:43:27 +0800

    feat: added new function to play the game in 5 rounds

--- script.js
+++ script.js
@@ -62,12 +62,31 @@ function playRound(humanChoice, computerChoice){
     else {
         console.log("Round Invalid");
     }
+    console.log("Scoreboard ", "Player:",humanScore,"Computer:", computerScore);
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log("You chose: ",humanSelection);
-console.log("Computer chose: ",computerSelection);
+function playGame(){
+    for (let i=1;i<=5;i++){
+        console.log("Round ",i)
+        const humanSelection = getHumanChoice();
 
-playRound(humanSelection, computerSelection);
-console.log("Scoreboard ", "Player:",humanScore,"Computer:", computerScore);
+        if (humanSelection === ""){
+            console.log("Match forfeited")
+        }
+        const computerSelection = getComputerChoice();
+        console.log("You chose: ",humanSelection);
+        console.log("Computer chose: ",computerSelection);
+        playRound(humanSelection, computerSelection);
+    }
+    console.log("Game OVER!")
+    if (humanScore>computerScore){
+        console.log("You won the game!");
+    }
+    else if (humanScore<computerScore){
+        console.log("You lose! Computer won!")
+    }
+    else console.log("Tiebreaker!")
+    console.log("Final Score --- ", "You:",humanScore,"Computer:",computerScore);
+}
+
+playGame();
